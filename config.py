c1be050daa465b1c8283175e6691a0c984d3715b
Sanjana T S <150058865+sanjanats7@users.noreply.github.com>
2025-03-10 10:42:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD", "Viridian@7"))
-DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
-DB_NAME = os.getenv("DB_NAME", "classicmodels")
+DB_USER = os.getenv("DB_USER", "")
+DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD", ""))
+DB_HOST = os.getenv("DB_HOST", "")
+DB_NAME = os.getenv("DB_NAME", "")
 DB_PORT = os.getenv("DB_PORT", "3306")
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
